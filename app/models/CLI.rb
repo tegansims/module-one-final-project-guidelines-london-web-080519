9fd1dc938c9bd79fb63f0d1683cf9fd6e64437c8
github username <github email address>
2019-08-20 16:32:21 +0100

    fixed bug in random name generator and like it or not method

--- app/models/CLI.rb
+++ app/models/CLI.rb
@@ -64,63 +64,91 @@ class CLI
         @prompt.select("here are your options:", options)
      end
 #  #------------- SHOW NAMES ---------------------- 
- def self.find_picks(user, yn)
-    picks = Pick.where(user_id: user.id, yes_or_no: yn)
-    picks_array = picks.map {|pick| pick.name.name}
-    
- end
+    def self.find_picks(user, yn)
+        picks = Pick.where(user_id: user.id, yes_or_no: yn)
+        picks_array = picks.map {|pick| pick.name.name}
+        
+    end
 
- def self.show_picks(user,yn)
-    print find_picks(user, yn)
- end 
-
-
- 
- def self.find_partner
-    partner_username= @prompt.ask("What is your partner's username?")
-    @partner_user = User.find_by(username: partner_username)
- end
-
- def self.show_matches
-    find_partner
-    binding.pry
-    find_picks(@current_user, "Y") & find_picks(@partner_user, "Y")
-    puts "TO BUILD: here are all your matches"
- end 
-
- #------------- RANDOM NAME ---------------------- 
-
- 
-    def self.get_random_name 
-        Name.all.sample
-    end 
-
-    def self.random_name_all 
-        random_name = get_random_name 
-        if Pick.find_by(user_id: @current_user.id, name_id: random_name.id)
-            self.random_name_all
-            puts "re-running"     
+    def self.show_picks(user,yn)
+        if yn == "Y"
+        puts "Here are all your picks: #{find_picks(user, yn)}"
         else 
-            puts "your name is #{random_name.name}"
+        puts "Here are all your rejects: #{find_picks(user, yn)}"
         end 
     end 
 
+    
+    def self.find_partner
+        partner_username= @prompt.ask("What is your partner's username?")
+        @partner_user = User.find_by(username: partner_username)
+    end
+
+    def self.show_matches
+        find_partner
+        matches = find_picks(@current_user, "Y") & find_picks(@partner_user, "Y")
+        puts "Here are all your matches: #{matches}"
+    end 
+
+    #------------- RANDOM NAME ---------------------- 
+
+    def self.random_name_menu
+        # create array of menu choices
+        #ask for gender in a menu
+        prompt = TTY::Prompt.new
+        options = [
+            {"Female names" => -> do self.random_name_all("Female") end},
+            {"Male names" => -> do self.random_name_all("Male") end},
+            {"Genderless names" => -> do self.random_name_all(nil) end},
+        ]
+        prompt.select("What gender are you looking for?", options)
+        # define two methods for our three options
+        # call it
+    end
 
 
-  
-    # #choose name and put in pick folder as yes
-    # def pick_name_yes(user_id, name_id, comment, rating, yes_or_no = "yes")
-    #     Pick.create(user_id, name_id, comment, rating)
-    # end 
-
-    # def pick_name_no(user_id, name_id, comment, rating, yes_or_no = "no")
-    #     Pick.create(user_id, name_id, comment, rating)
-    # end 
+    def self.random_name_by_gender(gender)
+        if gender
+            @random_name = Name.where(gender:gender).sample
+        else
+            @random_name = Name.all.sample
+        end
+    end
 
 
-    # method to ask user if they want to continue choosing 
-    # if yes, call random_name_gender again 
-    # if no take them back to main menu 
+    def self.random_name_all(gender) 
+        random_name_by_gender(gender)
+        if Pick.find_by(user_id: @current_user.id, name_id: @random_name.id)
+            puts "re-running"
+            self.random_name_all(gender)     
+        else 
+            puts "your name is #{@random_name.name}"
+            like_or_not
+        end 
+    end 
+
+    # like_or_not => 
+    def self.like_or_not
+        options = [
+            {"Yes" => -> do self.pick_name("Y") end},
+            {"No" => -> do self.pick_name("N") end},
+            {"Take me back to the menu" => -> do self.home_menu end}
+        ]
+        @prompt.select("Do you like this name?", options)
+    end
+
+    #choose name and put in pick folder as yes
+    def self.pick_name(yn)
+        Pick.create(user_id: @current_user.id,name_id: @random_name.id, yes_or_no: yn)
+    end 
+
+
+    def self.get_random_name
+        random_name_menu
+        # random_name_all(gender)
+        # like_or_not
+    end
+
 
 
 #---------------------------DELETE ACCOUNT HERE---------------------------------------------
@@ -150,9 +178,9 @@ class CLI
     def self.methods 
         self.greet
         self.check_if_have_login_and_create
-        self.home_menu
-        # self.random_name_all
-        # self.delete_account
+        while @current_user
+            self.home_menu
+        end
     end 
 
 
